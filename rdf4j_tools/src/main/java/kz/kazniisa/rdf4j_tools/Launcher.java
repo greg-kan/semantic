31a32a99419a3a119051863e82afae3fde191d38
grigoriy.kanavenko <g.kanavenko@gmail.com>
2019-10-30 16:35:30 +0600

    rdf4j_tools is starting to evaluate

--- rdf4j_tools/src/main/java/kz/kazniisa/rdf4j_tools/Launcher.java
+++
@@ -1,7 +0,0 @@
-package kz.kazniisa.rdf4j_tools;
-
-public class Launcher {
-    public static void main(String[] args) {
-        System.out.println("Begin creating Classifier ontology...");
-    }
-}
